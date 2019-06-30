d74413df922e6a747c88703c5c7eb3a8eb77b0f7
Andres Concha <andresconcha@pop-os.localdomain>
2019-06-30 00:01:38 -0500

    Admin dashboard styled

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,31 +2,50 @@
   <section>
     <Header></Header>
     <div class="main-container container-fluid">
-      <h1>Administrador</h1>
-      <router-link v-if="showButton('challenge_texts')" tag="b-button" class="button btnStyle" to="/challenge_texts">
-        <font-awesome-icon icon="font"/> Textos Retos
-      </router-link>
-      <router-link v-if="showButton('users')" tag="b-button" class="button btnStyle" to="/users">
-        <font-awesome-icon icon="users"/> Usuarios
-      </router-link>
-      <router-link v-if="showButton('challenges')" tag="b-button" class="button btnStyle" to="/challenges">
-        <font-awesome-icon icon="th-large"/> Retos
-      </router-link>
-      <router-link v-if="showButton('ideas')" tag="b-button" class="button btnStyle" to="/ideas/all">
-        <font-awesome-icon icon="lightbulb"/> Ideas
-      </router-link>
-      <router-link v-if="showButton('comments')" tag="b-button" class="button btnStyle" to="/comments/all">
-        <font-awesome-icon icon="comments" /> Comentarios
-      </router-link>
-      <router-link v-if="showButton('stages')" tag="b-button" class="button btnStyle" to="/stages">
-        <font-awesome-icon icon="project-diagram"/> Etapas
-      </router-link>
-      <router-link v-if="showButton('votes')" tag="b-button" class="button btnStyle" to="/picked_ideas_results">
-        <font-awesome-icon icon="vote-yea"/> Votaciones
-      </router-link>
-      <router-link v-if="showButton('surveys')" tag="b-button" class="button btnStyle" to="/stages">
-        <font-awesome-icon icon="poll"/> Encuestas
-      </router-link>
+      <b-row>
+        <b-col align="center">
+          <h1>Panel Adminsitrativo</h1>
+          <br>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col align="center" class="border-right border-left">
+          <h3>Etapas</h3>
+          <router-link v-if="showButton('stages')" tag="b-button" class="button btnStyle" to="/stages">
+            <font-awesome-icon icon="project-diagram"/> Etapas
+          </router-link>
+        </b-col>
+        <b-col align="center" class="border-right border-left">
+          <h3>Usuarios</h3>
+          <router-link v-if="showButton('users')" tag="b-button" class="button btnStyle" to="/users">
+            <font-awesome-icon icon="users"/> Usuarios
+          </router-link>
+        </b-col>
+        <b-col align="center" class="border-right border-left">
+          <h3>Contenidos</h3>
+          <router-link v-if="showButton('challenge_texts')" tag="b-button" class="button btnStyle" to="/challenge_texts">
+            <font-awesome-icon icon="font"/> Textos Retos
+          </router-link>
+          <router-link v-if="showButton('challenges')" tag="b-button" class="button btnStyle" to="/challenges">
+            <font-awesome-icon icon="th-large"/> Retos
+          </router-link>
+          <router-link v-if="showButton('ideas')" tag="b-button" class="button btnStyle" to="/ideas/all">
+            <font-awesome-icon icon="lightbulb"/> Ideas
+          </router-link>
+          <router-link v-if="showButton('comments')" tag="b-button" class="button btnStyle" to="/comments/all">
+            <font-awesome-icon icon="comments" /> Comentarios
+          </router-link>
+        </b-col>
+        <b-col align="center" v-if="currentStage.number != 0" class="border-right border-left">
+          <h3>Resultados</h3>
+          <router-link v-if="showButton('votes')" tag="b-button" class="button btnStyle" to="/picked_ideas_results">
+            <font-awesome-icon icon="vote-yea"/> Votaciones
+          </router-link>
+          <router-link v-if="showButton('surveys')" tag="b-button" class="button btnStyle" to="/stages">
+            <font-awesome-icon icon="poll"/> Encuestas
+          </router-link>
+        </b-col>
+      </b-row>
     </div>
   </section>
 </template>
@@ -107,7 +126,7 @@ export default {
   width: 150px;
 }
 .main-container {
-  margin-top: 90px;
+  margin-top: 100px;
 }
 </style>
 
